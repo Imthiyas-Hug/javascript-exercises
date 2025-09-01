fcf6aab3e557a485ecbcfa389a25e4fcef1c071d
Imthiyas-Hug <imthiyashugi@gmail.com>
2025-09-01 12:46:02 +0530

    Modify files from 12 to 16
    Completed the Exercises

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    let age = [];
+    let currentDate = new Date();
+    let currentYear = currentDate.getFullYear();
 
+    people.forEach((person, index) => {
+        if (person.yearOfDeath == undefined) {
+            person.yearOfDeath = currentYear;
+        }
+        age[index] = person.yearOfDeath - person.yearOfBirth;
+    })
+    let max = Math.max(...age)
+
+    const oldest = people.filter(checkOldest);
+
+    function checkOldest(person) {
+        return (person.yearOfDeath - person.yearOfBirth) === max;
+    }
+    return oldest[0];
 };
 
 // Do not edit below this line
